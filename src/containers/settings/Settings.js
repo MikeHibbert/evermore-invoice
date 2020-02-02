28c33c99871013a3de561d114f5786b68b0a755f
mhibbert <hibbert.michael@gmail.com>
2020-02-02 08:55:10 +0000

    added more settings

--- src/containers/settings/Settings.js
+++ src/containers/settings/Settings.js
@@ -5,7 +5,11 @@ class Settings extends Component {
         name: '',
         api_key: '',
         email_from: '',
-        currency_symbol: '$'
+        currency_type: 'Dollars',
+        currency_symbol: '$',
+        default_invoice_note: '',
+        tax_code: '',
+        tax_rate: 0.0
     }
 
     constructor(props) {
@@ -18,13 +22,22 @@ class Settings extends Component {
         const api_key = localStorage.getItem('evermore-invoice-sendgrid-api-key', null);
         const email_from = localStorage.getItem('evermore-invoice-sendgrid-email-from', null);
         const currency_symbol = localStorage.getItem('evermore-invoice-sendgrid-currency-symbol', null);
+        const currency_type = localStorage.getItem('evermore-invoice-sendgrid-currency-type', null);
+        const default_invoice_note = localStorage.getItem('evermore-invoice-default-invoice-note', null);
+        const tax_code = localStorage.getItem('evermore-invoice-default-tax-code', null);
+        const tax_rate = localStorage.getItem('evermore-invoice-default-tax-rate', null);
 
         const state = {};
 
         if(name) state['name'] = name;
         if(api_key) state['api_key'] = api_key;
         if(email_from) state['email_from'] = email_from;
+        if(currency_type) state['currency_type'] = currency_type;
         if(currency_symbol) state['currency_symbol'] = currency_symbol;
+        if(default_invoice_note) state['default_invoice_note'] = default_invoice_note;
+        if(tax_code) state['tax_code'] = tax_code;
+        if(tax_rate) state['tax_rate'] = tax_rate;
+
 
         this.setState(state);
     }
@@ -33,9 +46,27 @@ class Settings extends Component {
         localStorage.setItem('evermore-invoice-name', this.state.name);
         localStorage.setItem('evermore-invoice-sendgrid-api-key', this.state.api_key);
         localStorage.setItem('evermore-invoice-sendgrid-email-from', this.state.email_from);
+        localStorage.setItem('evermore-invoice-sendgrid-currency-type', this.state.currency_type);
         localStorage.setItem('evermore-invoice-sendgrid-currency-symbol', this.state.currency_symbol);
+        localStorage.setItem('evermore-invoice-sendgrid-tax-code', this.state.tax_code);
+        localStorage.setItem('evermore-invoice-sendgrid-tax-rate', this.state.tax_rate);
 
         this.props.addSuccessAlert("You settings have been saved locally.");
+    }
+
+    OnExport() {
+        const settings = JSON.stringify(this.state);
+
+        console.log(settings);
+    }
+
+    OnImport() {
+        const state = '';
+        const settings = JSON.parse(state);
+
+        this.setState(settings);
+
+        this.OnSave();
     }
 
     OnChange(event) {
@@ -111,16 +142,66 @@ class Settings extends Component {
                                                 <div className="form-block-body">
                                                     <div className="form-group">
                                                         <input type="text" className="form-control" 
-                                                            id="currency_symbol" name="currency_symbol" placeholder="API key" 
+                                                            id="currency_type" name="currency_type" placeholder="Currency Type" 
+                                                            value={this.state.currency_type} 
+                                                            onChange={(e) => {this.OnChange(e)}}
+                                                            />
+                                                    </div>  
+                                                    <div className="form-group">
+                                                        <input type="text" className="form-control" 
+                                                            id="currency_symbol" name="currency_symbol" placeholder="Currency Symbol" 
                                                             value={this.state.currency_symbol} 
                                                             onChange={(e) => {this.OnChange(e)}}
                                                             />
                                                     </div>  
-                                                    <button type="button" id="submit" 
-                                                        onClick={this.OnSave.bind(this)} name="submit" 
-                                                        className="btn btn-primary pull-right">Update settings</button>                   
+                                                                    
                                                 </div>
-                                                
+                                                <div className="form-block-header">
+                                                    <h5>Invoice Settings</h5>
+                                                </div>
+                                                <div className="form-block-body">
+                                                    <div className="form-group">
+                                                        <textarea type="text" className="form-control" 
+                                                            id="currency_symbol" name="currency_symbol" placeholder="Default Invoice Note (things to remind your client about)" 
+                                                            value={this.state.default_invoice_note} 
+                                                            onChange={(e) => {this.OnChange(e)}}
+                                                            ></textarea>
+                                                    </div>  
+                                                    <div className="form-group">
+                                                        <input type="text" className="form-control" 
+                                                                id="tax_code" name="tax_code" placeholder="Tax Code/Number" 
+                                                                value={this.state.tax_code} 
+                                                                onChange={(e) => {this.OnChange(e)}}
+                                                                />   
+                                                        <label style={{marginTop: '5px'}}>Tax Rate</label>    
+                                                        <input type="number" className="form-control" 
+                                                                id="tax_rate" name="tax_rate" placeholder="Rate of tax (default 0.00%)" 
+                                                                pattern="([0-9]{1,3}).([0-9]{1,3})"
+                                                                step='0.01'
+                                                                value={this.state.tax_rate} 
+                                                                onChange={(e) => {this.OnChange(e)}}
+                                                                />       
+                                                    </div>
+                                                </div>
+                                                <div className="form-block-body">
+                                                    <button type="button" id="submit" 
+                                                            onClick={this.OnSave.bind(this)} name="submit" 
+                                                            className="btn btn-primary ">Update settings</button>  
+                                                             
+                                                </div>
+
+                                                <div className="form-block-header">
+                                                    <h5>Options</h5>
+                                                </div>
+                                                <div className="form-block-body">
+                                                    <button type="button" id="submit" 
+                                                            onClick={this.OnExport.bind(this)} name="submit" 
+                                                            className="btn btn-warning" style={{marginRight: '5px'}}>Export settings</button>  
+                                                    <button type="button" id="submit" 
+                                                            onClick={this.OnImport.bind(this)} name="submit" 
+                                                            className="btn btn-success ">Import settings</button> 
+                                                             
+                                                </div>
                                             </div>
                                         </form>
                                     </div>
