dde55ac810ef70186d486a8e7de18918523441a4
Epixylium <michael.h.hibbert@gmail.com>
2021-02-01 12:01:32 +0000

    Large WIP
    lots of things to fix but started working on timesheet integration for creation of invoices

--- src/containers/invoices/InvoiceNew.js
+++ src/containers/invoices/InvoiceNew.js
@@ -1,17 +1,34 @@
 import React, {Component} from 'react';
-import { getTSheets } from './helpers';
+import { getTSheetsGQL } from './helpers';
 
 
 class InvoiceNew extends Component {
-    onImport () {
-        getTSheets()
+    state = {
+        timesheets: null
+    }
+    async onImport () {
+        this.setState({timesheets: await getTSheetsGQL()});
     }
        render() {
            return (
-            <div className="main-content">
-
-                <button className="btn btn-default form-control" onClick={() => {this.onImport()}}>Load Timelord Timesheets</button>
-            </div>
+                <div className="main-content">
+                    <button className="btn btn-default form-control" onClick={() => {this.onImport()}}>Load Timelord Timesheets</button>
+                    <table className="table table-align-middle border-bottom mb-6">
+                        <thead>
+                            <tr>
+                                <th colSpan='3'>Timesheet Collection</th>
+                            </tr>
+                            <tr>
+                                <th>Start Time</th>
+                                <th>End Time</th>
+                                <th>Client</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {this.state.timesheets}
+                        </tbody>
+                    </table>
+                </div>
            );
        }
 }
--- src/containers/invoices/helpers.js
+++ src/containers/invoices/helpers.js
@@ -1,5 +1,6 @@
 import arweave from '../../arweave-config';
 import settings from '../../app-config';
+const axios = require('axios')
 
 export async function getTSheets() {
     const wallet_address = sessionStorage.getItem('AR_Wallet', null);
@@ -48,7 +49,98 @@ export async function getTSheets() {
         tsheets.push(timesheet);
     }
 
-    debugger;
 
     return tsheets;
+}
+
+export async function getTSheetsGQL() {
+    
+    const address = sessionStorage.getItem('AR_Wallet', null);
+
+    let cursor = '';
+    let hasNextPage = true;
+    const transactions = [];
+
+    while(hasNextPage) {
+        const query = `{
+            transactions(
+                owners: ["${address}"]
+                tags: [
+                {
+                    name: "App",
+                    values: ["Timelord"]
+                },
+                ]
+                after: "${cursor}"
+                first: 100    ) {
+                pageInfo {
+                    hasNextPage
+                }
+                edges {
+                    cursor
+                    node {
+                          owner {
+                          address
+                        }
+                        id
+                        tags {
+                            name
+                            value
+                        }
+                        block {
+                            timestamp
+                            height
+                        }
+                    }
+                }
+            }
+        }`;
+
+        const response = await axios.post("https://arweave.net/graphql", {
+            operationName: null,
+            query: query,
+            variables: {}
+        });
+        
+        if(response.status == 200) {
+            const data = response.data.data;
+
+            for(let i in data.transactions.edges) {
+                const item = data.transactions.edges[i].node;
+
+                const result = await arweave.transactions.getData(item.id , {decode: true, string: true});
+
+                item['data'] = JSON.parse(result);
+
+                Object.keys(item.data).map(key => {
+                    const data = item.data[key];
+
+                    if(key == 'start' || key == 'finish') {
+                        item[key] = new Date(data);
+                    }
+                });
+
+                try {
+                    const client_data = await arweave.transactions.getData(item.data.clientid , {decode: true, string: true});
+
+                    item['client'] = JSON.parse(client_data);
+                } catch (e) {
+                    console.error(`${item.data.clientid} created error ${e}`);
+                }
+                
+
+                transactions.push(item);
+            }
+
+            hasNextPage = data.transactions.pageInfo.hasNextPage;
+
+            if(hasNextPage) {
+                cursor = data.transactions.edges[data.data.transactions.edges.length - 1].cursor;
+            }
+        } else {
+            hasNextPage = false;
+        }
+    }
+
+    return transactions;
 }
