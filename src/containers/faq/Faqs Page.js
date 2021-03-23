89b388fe5c56aabadcf8e1316a6849bda4d059a3
MikeHibbert <hibbert.michael@gmail.com>
2021-03-23 09:49:47 +0000

    adjusted css for faqs

--- src/containers/faq/Faqs Page.js
+++ src/containers/faq/Faqs Page.js
@@ -9,11 +9,7 @@ import { validate } from 'react-email-validator';
 
 const customStyles = {
     content: {
-        top: '50%',
-        left: '50%',
-        right: 'auto',
-        bottom: 'auto',
-        float: 'left',
+        display: 'none'
     }
 };
 
